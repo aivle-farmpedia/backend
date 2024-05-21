cd0162e665b23ea9dad0cec4d95093a24c114e78
pwrwpw <dlgustjr9912@gmail.com>
2024-05-21 22:00:19 +0900

    refact : 오타 수정

--- src/main/java/com/farm/pedia/FarmPediaApplication.java
+++ src/main/java/com/farm/pedia/FarmPediaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParmPediaApplication {
+public class FarmPediaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParmPediaApplication.class, args);
+		SpringApplication.run(FarmPediaApplication.class, args);
 	}
 
 }
