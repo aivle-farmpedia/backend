be48daa28f27f747132ef7c50238c4e0e0d171df
pwrwpw <dlgustjr9912@gmail.com>
2024-06-09 00:24:24 +0900

    refact : xml에서 쿼리를 관리하도록 수정 및 사용하지 않는 메서드 제거

--- src/main/java/com/farm/pedia/searchHistory/mapper/SearchHistoryMapper.java
+++ src/main/java/com/farm/pedia/searchHistory/mapper/SearchHistoryMapper.java
@@ -3,17 +3,8 @@ package com.farm.pedia.searchHistory.mapper;
 import com.farm.pedia.searchHistory.domain.SearchHistory;
 import org.apache.ibatis.annotations.*;
 
-import java.util.List;
-
 @Mapper
 public interface SearchHistoryMapper {
 
-    @Insert("INSERT INTO search_history (keyword, searched_at, user_id) VALUES (#{keyword}, #{searchedAt}, #{userId})")
     void save(SearchHistory searchHistory);
-
-    @Select("SELECT * FROM search_history WHERE user_id = #{userId} ORDER BY searched_at DESC LIMIT 5")
-    List<SearchHistory> findRecentByUserId(Long userId);
-
-    @Delete("DELETE FROM search_history WHERE id = #{id} AND user_id = #{userId}")
-    void deleteByIdAndUserId(@Param("id") Long id, @Param("userId") Long userId);
 }
