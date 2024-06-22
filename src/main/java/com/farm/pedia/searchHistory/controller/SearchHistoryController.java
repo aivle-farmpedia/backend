36c16b709be73e752d168e1ac048382c18dce6db
b-sujin <sjsw123456@gmail.com>
2024-06-23 01:30:19 +0900

    feat : 기존 검색어 저장 기능 수정 및 연관검색어 기능 추가 - 검색 시 검색어 Redis에 저장 후 검색 실시 - 검색어가 존재하지 않으면 메시지 출력 - 검색어가 존재할 시 카테고리 명 출력

--- src/main/java/com/farm/pedia/searchHistory/controller/SearchHistoryController.java
+++ src/main/java/com/farm/pedia/searchHistory/controller/SearchHistoryController.java
@@ -1,6 +1,7 @@
 package com.farm.pedia.searchHistory.controller;
 
 import com.farm.pedia.auth.config.UserLogin;
+import com.farm.pedia.searchHistory.domain.SearchResults;
 import com.farm.pedia.searchHistory.service.SearchHistoryService;
 import com.farm.pedia.user.domain.User;
 
@@ -9,6 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,17 +26,29 @@ public class SearchHistoryController {
     private final SearchHistoryService searchHistoryService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void saveSearchKeyword(@UserLogin User user, @Valid @NotBlank @RequestParam String keyword) {
+    public ResponseEntity<String> saveAndPerformSearch(@UserLogin User user, @Valid @NotBlank @RequestParam("keyword") String keyword) {
+        // 검색어 저장
         searchHistoryService.saveSearchKeyword(user.getId(), keyword);
-        log.info("User ID {}의 검색어 '{}'가 저장되었습니다.", user.getId(), keyword);
+        log.info("UserID '{}'의 검색어 [{}] 저장되었습니다.", user.getId(), keyword);
+
+        // 검색 실행
+        SearchResults results = searchHistoryService.performSearch(keyword);
+        if (results.getNames().isEmpty()) {
+            String message = "검색어 [" + keyword + "]은 존재하지 않습니다.";
+            log.info(message);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+
+        }
+        String formattedResults = "연관 검색어 : " + String.join(", ", results.getNames());
+        log.info("{}", formattedResults);
+        return new ResponseEntity<>(formattedResults, HttpStatus.OK);
     }
+
 
     @GetMapping("/recent")
     @ResponseStatus(HttpStatus.OK)
     public Set<String> getRecentSearchKeywords(@UserLogin User user) {
         Set<String> recentKeywords = searchHistoryService.getRecentSearchKeywords(user.getId());
-
         log.info("User ID {}의 최근 검색어를 조회하였습니다: {}", user.getId(), recentKeywords);
         return recentKeywords;
     }
--- src/main/java/com/farm/pedia/searchHistory/mapper/SearchHistoryMapper.java
+++ src/main/java/com/farm/pedia/searchHistory/mapper/SearchHistoryMapper.java
@@ -1,10 +1,17 @@
 package com.farm.pedia.searchHistory.mapper;
 
 import com.farm.pedia.searchHistory.domain.SearchHistory;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface SearchHistoryMapper {
 
     void save(SearchHistory searchHistory);
+
+    List<String> searchVarietyByName(@Param("keyword") String keyword);
+
+    List<String> searchCropsByVarietyName(@Param("keyword") String keyword);
 }
