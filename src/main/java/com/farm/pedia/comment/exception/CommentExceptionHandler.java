c07aaed94f9af3748fa50f576b133b12dadb27f4
pwrwpw <dlgustjr9912@gmail.com>
2024-06-23 18:44:59 +0900

    fix : 어노테이션 추가

--- src/main/java/com/farm/pedia/comment/exception/CommentExceptionHandler.java
+++ src/main/java/com/farm/pedia/comment/exception/CommentExceptionHandler.java
@@ -2,26 +2,28 @@ package com.farm.pedia.comment.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.farm.pedia.comment.exception.exceptions.CommentAlreadyDeletedException;
 import com.farm.pedia.comment.exception.exceptions.CommentAuthorOnlyException;
 import com.farm.pedia.comment.exception.exceptions.CommentNotFoundException;
 import com.farm.pedia.global.exception.AbstractExceptionHandler;
 
+@RestControllerAdvice
 public class CommentExceptionHandler extends AbstractExceptionHandler {
 
 	@ExceptionHandler(CommentNotFoundException.class)
-	public ResponseEntity<String>  handleCommentNotFoundException(CommentNotFoundException e) {
+	public ResponseEntity<String> handleCommentNotFoundException(CommentNotFoundException e) {
 		return handleNotFound(e);
 	}
 
 	@ExceptionHandler(CommentAlreadyDeletedException.class)
-	public ResponseEntity<String>  handleCommentAlreadyDeletedException(CommentAlreadyDeletedException e) {
+	public ResponseEntity<String> handleCommentAlreadyDeletedException(CommentAlreadyDeletedException e) {
 		return handleBadRequest(e);
 	}
 
 	@ExceptionHandler(CommentAuthorOnlyException.class)
-	public ResponseEntity<String>  handleCommentAuthorOnlyException(CommentAuthorOnlyException e) {
+	public ResponseEntity<String> handleCommentAuthorOnlyException(CommentAuthorOnlyException e) {
 		return handleForbidden(e);
 	}
 }
